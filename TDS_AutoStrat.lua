3edd97117c2db8f7fade8a209e63a23c8c5b681a
Naetl <109276229+Naetl@users.noreply.github.com>
2026-01-01 07:23:13 +0900

    Update TDS_AutoStrat.lua

--- TDS_AutoStrat.lua
+++ TDS_AutoStrat.lua
@@ -710,14 +710,14 @@ function TDS:Addons()
     if game_state ~= "GAME" then
         return false
     end
-    local code = "https://raw.githubusercontent.com/Naetl/Scripts/refs/heads/main/TDS_Equip.lua"
+    local url = "https://raw.githubusercontent.com/Naetl/Scripts/refs/heads/main/TDS_Equip.lua"
     --local url = "https://api.junkie-development.de/api/v1/luascripts/public/57fe397f76043ce06afad24f07528c9f93e97730930242f57134d0b60a2d250b/download"
-    --local success, code = pcall(game.HttpGet, game, url)
+    local success, code = pcall(game.HttpGet, game, url)
 
-    --[[
+    
     if not success then
         return false
-    end]]
+    end
 
     loadstring(code)()
 
